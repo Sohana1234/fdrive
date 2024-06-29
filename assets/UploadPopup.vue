66a03fff3b3e4264909e9775ec82fe1778df018b
Sohana Gupta <sohana.gupta@aristagroup.ne>
2024-06-29 18:43:14 +0530

    thirtheen commit

--- assets/UploadPopup.vue
+++ assets/UploadPopup.vue
@@ -92,7 +92,6 @@ async function uploadFile(file) {
         await multipartUpload(`${cwd}${uniqueDir}/${file.name}`,file,{
             headers,
             onUploadProgress: (progressEvent) => {
-			  console.log("progressEvent",progressEvent);
 			  if (progressEvent.total > 0) {
                 progressValue.value = Math.round((progressEvent.loaded * 100) / progressEvent.total);
 				}
@@ -109,10 +108,9 @@ async function uploadFile(file) {
         await axios.put(uploadUrl, file, {
         headers,
       onUploadProgress: (progressEvent) => {
-	  console.log("progressEventput",progressEvent);
-	  if (progressEvent.total > 0) {
+	 
         progressValue.value = Math.round((progressEvent.loaded * 100) / progressEvent.total);
-		}
+		
 		if (progressValue.value === 100) {
           setTimeout(() => {
             state.value = 3; 
