c24012db95d1292ac65579151ede1516a22c1171
Sohana Gupta <sohana.gupta@aristagroup.ne>
2024-06-30 18:23:48 +0530

    Fix the bucket api

--- functions/api/buckets.ts
+++ functions/api/buckets.ts
@@ -1,30 +1,28 @@
 import { S3Client } from "@/utils/s3";
 
-const NEW_ACCESS_KEY_ID = "8f70c6c6a8d964bec51abcf4b57022a4";
-const NEW_SECRET_ACCESS_KEY = "0fecc90f43f15f0230ee16b12ba84cd7e6580c61df576fa055c52cf58182c891";
-const NEW_CF_ACCOUNT_ID = "416c8dfd48d7017aa7d3dc38412356ca";
-const NEW_BUCKET_NAME = "uploader";
-const NEW_ENDPOINT = `https://${NEW_CF_ACCOUNT_ID}.r2.cloudflarestorage.com`;
-
 async function getCurrentBucket(context) {
-  const { request } = context;
+  const { request, env } = context;
   const url = new URL(request.url);
   const driveid = url.hostname.replace(/\..*/, "");
 
-  const client = new S3Client(NEW_ACCESS_KEY_ID, NEW_SECRET_ACCESS_KEY, NEW_ENDPOINT);
-  const bucketsResponse = await client.listBuckets();
+  if (!(await env[driveid].head("_$flaredrive$/CNAME")))
+    await env[driveid].put("_$flaredrive$/CNAME", url.hostname);
+
+  const client = new S3Client(env.NEW_ACCESS_KEY_ID, env.NEW_SECRET_ACCESS_KEY);
+  const bucketsResponse = await client.s3_fetch(
+    `https://${env.NEW_CF_ACCOUNT_ID}.r2.cloudflarestorage.com/`
+  );
   const bucketsText = await bucketsResponse.text();
   const bucketNames = [
     ...bucketsText.matchAll(/<Name>([0-9a-z-]*)<\/Name>/g),
   ].map((match) => match[1]);
-
   const currentBucket = await Promise.any(
     bucketNames.map(
       (name) =>
         new Promise<string>((resolve, reject) => {
           client
             .s3_fetch(
-              `${NEW_ENDPOINT}/${name}/_$flaredrive$/CNAME`
+              `https://${env.NEW_CF_ACCOUNT_ID}.r2.cloudflarestorage.com/${name}/_$flaredrive$/CNAME`
             )
             .then((response) => response.text())
             .then((text) => {
@@ -49,10 +47,11 @@ export async function onRequestGet(context) {
     if (url.searchParams.has("current")) return await getCurrentBucket(context);
 
     const client = new S3Client(
-      NEW_ACCESS_KEY_ID, NEW_SECRET_ACCESS_KEY,NEW_ENDPOINT
+      env.NEW_ACCESS_KEY_ID,
+      env.NEW_SECRET_ACCESS_KEY
     );
     return client.s3_fetch(
-      `https://${NEW_CF_ACCOUNT_ID}.r2.cloudflarestorage.com/`
+      `https://${env.NEW_CF_ACCOUNT_ID}.r2.cloudflarestorage.com/`
     );
   } catch (e) {
     return new Response(e.toString(), { status: 500 });
