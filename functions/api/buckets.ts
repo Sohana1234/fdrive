5ea23325e256ec5e5e18739d475e7856d90d405b
Sohana Gupta <sohana.gupta@aristagroup.ne>
2024-06-30 23:30:26 +0530

    checking

--- functions/api/buckets.ts
+++ functions/api/buckets.ts
@@ -10,7 +10,7 @@ async function getCurrentBucket(context) {
 
   const client = new S3Client(env.NEW_ACCESS_KEY_ID, env.NEW_SECRET_ACCESS_KEY);
   const bucketsResponse = await client.s3_fetch(
-    `https://${env.NEW_CF_ACCOUNT_ID}.r2.cloudflarestorage.com/`
+    `https://${env.NEW_CF_ACCOUNT_ID}.r2.cloudflarestorage.com`
   );
   const bucketsText = await bucketsResponse.text();
   const bucketNames = [
