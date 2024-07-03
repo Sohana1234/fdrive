20e5cdd07aa02ae7c49caadb735fdc08078ea2b1
devmobux <tchetcheherman@gmail.com>
2024-07-03 20:41:10 +0000

    testing12

--- functions/api/buckets.ts
+++ functions/api/buckets.ts
@@ -2,11 +2,7 @@ import { S3Client } from "@/utils/s3";
 
 async function getCurrentBucket(context) {
   const { request, env } = context;
-  env.NEW_ACCESS_KEY_ID='8f70c6c6a8d964bec51abcf4b57022a4';
-  env.NEW_SECRET_ACCESS_KEY='0fecc90f43f15f0230ee16b12ba84cd7e6580c61df576fa055c52cf58182c891';
-  env.NEW_ENDPOINT='https://416c8dfd48d7017aa7d3dc38412356ca.r2.cloudflarestorage.com';
-  env.NEW_CF_ACCOUNT_ID='416c8dfd48d7017aa7d3dc38412356ca';
-  env.BUCKET='uploader'
+
   const url = new URL(request.url);
   const driveid = url.hostname.replace(/\..*/, "");
 
@@ -14,7 +10,7 @@ async function getCurrentBucket(context) {
     await env[driveid].put("_$flaredrive$/CNAME", url.hostname);
 
   const client = new S3Client(env.NEW_ACCESS_KEY_ID, env.NEW_SECRET_ACCESS_KEY);
-  console.log("client",env);
+
   const bucketsResponse = await client.s3_fetch(
     `https://${env.NEW_CF_ACCOUNT_ID}.r2.cloudflarestorage.com/`
   );
@@ -48,14 +44,10 @@ async function getCurrentBucket(context) {
 export async function onRequestGet(context) {
   try {
     const { request, env } = context;
-    env.NEW_ACCESS_KEY_ID='8f70c6c6a8d964bec51abcf4b57022a4';
-    env.NEW_SECRET_ACCESS_KEY='0fecc90f43f15f0230ee16b12ba84cd7e6580c61df576fa055c52cf58182c891';
-    env.NEW_ENDPOINT='https://416c8dfd48d7017aa7d3dc38412356ca.r2.cloudflarestorage.com';
-    env.NEW_CF_ACCOUNT_ID='416c8dfd48d7017aa7d3dc38412356ca';
-    env.BUCKET='uploader'
+
     const url = new URL(request.url);
     if (url.searchParams.has("current")) return await getCurrentBucket(context);
-   
+
     const client = new S3Client(
       env.NEW_ACCESS_KEY_ID,
       env.NEW_SECRET_ACCESS_KEY
--- utils/bucket.ts
+++ utils/bucket.ts
@@ -1,4 +1,3 @@
-
 export function notFound() {
   return new Response("Not found", { status: 404 });
 }
@@ -6,11 +5,6 @@ export function notFound() {
 export function parseBucketPath(context): [any, string] {
   const { request, env, params } = context;
   const url = new URL(request.url);
-  env.NEW_ACCESS_KEY_ID='8f70c6c6a8d964bec51abcf4b57022a4';
-  env.NEW_SECRET_ACCESS_KEY='0fecc90f43f15f0230ee16b12ba84cd7e6580c61df576fa055c52cf58182c891';
-  env.NEW_ENDPOINT='https://416c8dfd48d7017aa7d3dc38412356ca.r2.cloudflarestorage.com';
-  env.NEW_CF_ACCOUNT_ID='416c8dfd48d7017aa7d3dc38412356ca';
-  env.BUCKET='uploader'
 
   const pathSegments = (params.path || []) as String[];
   const path = decodeURIComponent(pathSegments.join("/"));
